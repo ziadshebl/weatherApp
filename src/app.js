e884a5200cb4da2d0165f49dcfa3f35bfebbb115
ziadshebl <ziadshebl@gmail.com>
2020-06-19 11:44:25 +0200

    Modifyin typo error

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
-const geoCode = require('./utils/geoCode')
+const geoCode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
